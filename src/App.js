03d2814b33e958bd56b60340c0631fac2dd7fa79
pablo fabian <pablofabian.369@gmail.com>
2022-10-20 22:03:04 -0500

    uploading to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React, {Fragment} from 'react';
+// import './App.css';
+import "./styles/styles.scss";
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Reglas JSX:
+  // 1. Toda Etiqueta debe cerrar etiquetas (incluso img se cierra)
+  // 2.Expresiones JSX solo pueden tener 1 hijo directo, solo se puede ser 1 (ejemplo div)
+  // 3. Fragment se tien que importar y se utiliza para apoyasrse para devolover 2 elementos y violar la regla 1
+  // 4: Fragment => <> hijos </>
+  // 5: class pasa a ser => className
+  // 6: for pasa a ser => htmlFor
+  // 7. EStilos como objetos
+  // 8. NO if, else, while
+                  
+const App = () => {
+  return(
+    // Revisar la consola en busca de advertencias
+  <>
+    
+    {/* <div className="card" style={{width: "18rem"}}>
+      <img className="card-img-top" src="https://avatars.githubusercontent.com/u/6853419?s=200&v=4" alt="Card cap error" />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="http://localhost" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div> */}
+
+    <Curso />
+        
+
+     
+  </>
+  )
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'; 
+
+
+// import reportWebVitals from './reportWebVitals';
+
+
+// const root =  ReactDOM.createRoot(document.getElementById("root"));
+
+
+// // Sintaxixs
+// // const elemento = React.createElement(componente, propiedades,hijos)
+
+// Debe tener la primera letra en mayuscula y el signo de cierre < />
+// ReactDOM.render(<App />, root);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +26,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// reportWebVitals();
